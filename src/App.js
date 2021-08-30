a98d84ee60c884d5276c548ccf7aac46c23be475
Tom McKinney <tom@upstream.dev>
2021-08-29 23:21:27 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1 className="jumbo">Julie &amp; Tom</h1>
+
+        <div className="m-5">
+          <h2>September 17th, 2022</h2>
+          <h2>Plymouth, MA</h2>
+        </div>
+
+        <p className="text-base italic">
+          More details coming soon...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
